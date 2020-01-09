576dd6a562a01087829ef0d0b2ed10946b41ae2b
21700500Myeonghui <21700500@handong.edu>
2020-01-09 22:52:55 +0900

    Initial commit

---
+++ app/src/main/jni/jniMain.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 이명희 on 2020-01-09.
+//
+
+#include "jniMain.h"
+#include <com_example_jnitest_MainActivity.h>
+
+
---
+++ app/src/main/jni/jniMain.h
@@ -0,0 +1,8 @@
+//
+// Created by 이명희 on 2020-01-09.
+//
+
+#ifndef JNITEST_JNIMAIN_H
+#define JNITEST_JNIMAIN_H
+
+#endif //JNITEST_JNIMAIN_H
